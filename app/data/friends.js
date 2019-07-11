074edc4ba7e49632e8bbdbedb8ae19b3abf382cb
Surface <da.ga.mo.94@gmail.com>
2019-07-10 22:17:28 -0600

    FInally sort of done.

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+var friends = [
+    {
+        name: "Imaginary",
+        photo: "https://images.unsplash.com/photo-1558980664-10e7170b5df9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2251&q=80",
+        scores: [1,2,3,4,5,1,2,3,4,5]
+    },
+    {
+        name: "another",
+        photo: "https://images.unsplash.com/photo-1558980664-10e7170b5df9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2251&q=80",
+        scores: [1,5,3,2,5,1,2,3,3,2]
+    },
+    {
+        name: "lol",
+        photo: "https://images.unsplash.com/photo-1558980664-10e7170b5df9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2251&q=80",
+        scores: [1,1,1,1,1,1,1,3,1,1]
+    },
+    {
+        name: "hmm",
+        photo: "https://images.unsplash.com/photo-1558980664-10e7170b5df9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2251&q=80",
+        scores: [5,5,5,5,5,5,5,5,5,5]
+    },
+    {
+        name: "nope",
+        photo: "https://images.unsplash.com/photo-1558980664-10e7170b5df9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2251&q=80",
+        scores: [4,5,3,4,5,2,2,4,4,5]
+    },
+
+];
+
+module.exports = friends;
